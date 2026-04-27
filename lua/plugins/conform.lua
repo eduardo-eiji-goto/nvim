755559963e9b5c023002fb83b799f24bcc33d3cd
Eduardo Eiji Goto <eduardo.e.goto@gmail.com>
2026-04-27 20:31:29 -0300

    chore: add formating to js adjacent languages

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,13 @@ require("conform").setup({
 		lua = { "stylua" },
 		python = { "isort", "black" },
 		go = { "gofmt" },
+		html = { "prettierd", "prettier", stop_after_first = true },
+		css = { "prettierd", "prettier", stop_after_first = true },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		jsx = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		vue = { "prettierd", "prettier", stop_after_first = true },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()

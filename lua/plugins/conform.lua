bedbbd9e277005b00aaf5ae09b7a32a6dc2c8060
eduardo-eiji-goto <eduardo.e.goto@opencon.com.br>
2026-09-25 19:49:04 -0300

    chore: add json to format rules

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ require("conform").setup({
 		python = { "isort", "black" },
 		go = { "gofmt" },
 		html = { "prettierd", "prettier", stop_after_first = true },
+		json = { "prettierd", "prettier", stop_after_first = true },
 		css = { "prettierd", "prettier", stop_after_first = true },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "prettierd", "prettier", stop_after_first = true },

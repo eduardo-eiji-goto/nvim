0ccfcc8a00f38321448a3073308225fbde5fb7bc
eduardo-eiji-goto <eduardo.e.goto@opencon.com.br>
2026-09-25 20:00:36 -0300

    chore: format file

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,18 +10,18 @@ require("mini.snippets").setup()
 require("mini.notify").setup()
 
 require("mini.starter").setup({
-  header = [[Yesterday is history,
+	header = [[Yesterday is history,
 tomorrow is a mystery,
 but today is a gift, that is why it is called the present.
 
 - Oogway]],
-  items = {
-    { action = ":enew",                 name = "New File",    section = "" },
-    { action = ":Telescope find_files", name = "Find Files",  section = "" },
-    { action = ":Telescope live_grep",  name = "Grep Files",  section = "" },
-    { action = ":q",                    name = "Quit Neovim", section = "" },
-  },
-  footer = "",
+	items = {
+		{ action = ":enew", name = "New File", section = "" },
+		{ action = ":Telescope find_files", name = "Find Files", section = "" },
+		{ action = ":Telescope live_grep", name = "Grep Files", section = "" },
+		{ action = ":q", name = "Quit Neovim", section = "" },
+	},
+	footer = "",
 })
 
 -- Keymap Extra Config
